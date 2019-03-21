ab6b8c41457ddf1eb49c117f7a5404cfd4662a63
AllenLu <f2212671555@gmail.com>
2019-03-21 19:56:10 +0800

    Initial Commit

---
+++ clashRoyaleIntro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  clashRoyaleIntro
+//
+//  Created by 陸顥壬 on 2019/3/21.
+//  Copyright © 2019 陸顥壬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
